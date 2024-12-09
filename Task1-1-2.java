1221b095a9befacbc41a1ebab2d299ea97b67a73
Peleeepe <philiprobinas.ict@gmail.com>
2024-12-09 08:10:46 +0000

    Done adding output

--- Task1-1-2.java
+++ Task1-1-2.java
@@ -1,18 +1,16 @@
 class task {
     public static void main(String[] args) {
         
-        byte b = 72; 
-        byte r = 114;       
+        byte b = 72;       
         short s = 3;        
         int i = 1;         
-        byte zero = 0; 
-        char w = 'w';       
+        byte zero = 0;       
         char d = 'd';
         float f = 2.0f;     
         boolean bool = true; 
 
         
-        String output = "" + (char)b + s + i + i + zero + " " + w + zero + (char)r + i + d + " " + f + " " + bool;
+        String output = "" + (char)b + s + i + i + zero + " " + "w" + zero + "r" + i + d + " " + f + " " + bool;
 
         
         System.out.println(output); 
