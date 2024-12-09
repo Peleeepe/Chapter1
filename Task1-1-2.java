16324d778aa1d9686d89881798ff76272bd091ad
Peleeepe <philiprobinas.ict@gmail.com>
2024-12-09 08:02:36 +0000

    Done adding output

--- Task1-1-2.java
+++ Task1-1-2.java
@@ -1,1 +1,20 @@
+class task {
+    public static void main(String[] args) {
+        
+        byte b = 72; 
+        byte r = 114;       
+        short s = 3;        
+        int i = 1;         
+        byte zero = 0; 
+        char w = 'w';       
+        char d = 'd';
+        float f = 2.0f;     
+        boolean bool = true; 
 
+        
+        String output = "" + (char)b + s + i + i + zero + " " + w + zero + (char)r + i + d + " " + f + " " + bool;
+
+        
+        System.out.println(output); 
+    }
+}
